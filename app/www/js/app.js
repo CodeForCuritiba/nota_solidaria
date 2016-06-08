d434f1f29e6cfec267c2c80d63058f76c3bd384d
Thierry Delbart <thierry@thde.pro>
2016-06-08 15:52:00 -0300

    Nota list in left menu + manual input format

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -42,12 +42,16 @@ angular.module('app', ['ionic','ngCordova'])
 
 .controller('AppCtrl', ['$scope', '$window', '$ionicModal', '$timeout', function($scope, $window, $ionicModal, $timeout) {
 
-    if ($window.localStorage && $window.localStorage.getItem('user')) $scope.user = JSON.parse($window.localStorage.getItem('user'));
-
     $scope.updateProfile = function(user){
       $window.localStorage.setItem('user', JSON.stringify(user)); 
       console.log(user);
     };
+
+    $scope.hasNotas = function(user) {
+      return Object.keys(user.notas).length > 0;
+    }
+
+    if ($window.localStorage && $window.localStorage.getItem('user')) $scope.user = JSON.parse($window.localStorage.getItem('user'));
 
 }])
 
@@ -84,7 +88,7 @@ angular.module('app', ['ionic','ngCordova'])
             $scope.updateProfile($scope.user);
 
             var alertPopup = $ionicPopup.alert({
-                 title: 'Obrigado !',
+                 title: 'Obrigado por sua doação!',
                  template: '<img src="http://lorempixel.com/200/200/people/" />'
             });
           }
@@ -96,16 +100,21 @@ angular.module('app', ['ionic','ngCordova'])
 
             if (barcodeData) {
 
+              // TODO: better testing to see if QR Code OK
               if ( (barcodeData["cancelled"] == false) && barcodeData['text']) {
                 url = barcodeData['text'];
                 donateFromUrl(url);
+              } else {
+                alert(JSON.stringify(barcodeData));
               }
+
             }
 
         }, function(error) {
             alert(JSON.stringify(error));
         });
       } else {
+        // Fake NF to test behavior
         url = "http://www.dfeportal.fazenda.pr.gov.br/dfe-portal/rest/servico/consultaNFCe?chNFe=41160579430682011400650010005451821005451825&nVersao=100&tpAmb=1&dhEmi=323031362d30352d31345431323a33303a32312d30333a3030&vNF=107.59&vICMS=0.50&digVal=32335937666347754b67564c504b50505369312b535679496b314d3d&cIdToken=000002&cHashQRCode=AD63C9270BBE2A05AF9C885B9FB563B4E28B3265";
         donateFromUrl(url);
       }
@@ -124,8 +133,23 @@ angular.module('app', ['ionic','ngCordova'])
     $state.go('app.scan');
   }
 
-
 }])
 
+.directive('notaChave', function() {
+  return {
+    require: 'ngModel',
+    link: function (scope, element, attr, ngModelCtrl) {
+      function fromUser(text) {
+        var transformedInput = text.replace(/[^0-9]/g, '').replace(/(.{4})/g, '$1 ').trim();;
+        if(transformedInput !== text) {
+            ngModelCtrl.$setViewValue(transformedInput);
+            ngModelCtrl.$render();
+        }
+        return transformedInput;
+      }
+      ngModelCtrl.$parsers.push(fromUser);
+    }
+  }; 
+});
 ;
 
